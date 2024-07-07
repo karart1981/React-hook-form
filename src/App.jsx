62a011f19b47ee2c213d39b82c182320b8c93c3e
karart1981 <karapart1981@gmail.com>
2024-07-07 11:55:45 +0400

    I created project with react-hook-form

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import { AddUser } from './components/AddUser'
+import { UserList } from './components/UserList'
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
+function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    axios
+    .get("http://localhost:3004/users")
+    .then(res=> {
+      setUsers(res.data)
+    })
+    
+  }, []);
+
+  const addItem = obj => {
+    setUsers([...users, obj])
+    toast.success("New user has been added successfully");
+  }
+
+  return (
+    <div className='row'>
+      <ToastContainer />
+      <AddUser
+        onAdd={addItem}
+      />
+      <UserList
+        users = {users}
+      />
+    </div>
+  )
+}
+
+export default App
